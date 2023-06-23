115eeac6e2a2073194711e71f26faa575c5fadf7
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-23 16:09:58 -0300

    Add project files.

---
+++ ItemDataApi/Controllers/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using ItemDataApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItemDataApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ItemController : ControllerBase
+{
+
+    private static List<Item> items = new List<Item>();
+    private static int id = 0;
+
+    [HttpPost]
+    public void CreateItem([FromBody] Item item)
+    {
+        item.Id = id++;
+        items.Add(item);
+    }
+
+    [HttpGet]
+    public IEnumerable<Item> GetItems([FromQuery]int skip = 0, [FromQuery]int take = 10)
+    {
+        return items.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Item? GetItemById(int id)
+    {
+        return items.FirstOrDefault(item => item.Id == id);
+    }
+}
---
+++ ItemDataApi/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItemDataApi.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "The name is required.")]
+    [MaxLength(255, ErrorMessage = "Name is too long.")]
+    public string Name { get; set; }
+
+    [Required]
+    public string Recipe { get; set; }
+
+    [Required]
+    public string CraftableIn { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "Ivalid crafting time.")]
+    public double CraftingTime { get; set; }
+
+    [Required]
+    [Range(1, 200, ErrorMessage = "Max stack size is 200.")]
+    public int StackSize { get; set; }
+}
